bf5bc743a92535c2dafb457345a535fa9602bd38
doundowebsite-a11y <doundowebsite@gmail.com>
2026-04-01 13:42:43 -0400

    Delete src directory

--- src/assets/icons/IconCardBack.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Svg, { Rect, Path } from 'react-native-svg';
-
-export const IconCardBack = ({ size = 24, color = '#256af4' }) => (
-  // Providing a stylized card back if the SVG isn't cleanly loading as a React component.
-  // Actually, let's just use the SVG file.
-  null
-);
